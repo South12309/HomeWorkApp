703b630a79c0555d01d52fcd96e7a3c153521735
South <south123@mail.ru>
2021-12-28 09:55:39 +0300

    HomeWork 3

--- src/main/java/com/geekbrains/HomeWorkApp.java
+++ src/main/java/com/geekbrains/HomeWorkApp.java
@@ -1,45 +1,6 @@
 package com.geekbrains;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {
-        
-    }
 
-    public static boolean isSumBetween10And20(int a, int b) {
-        int sum = a+b;
-        if (sum>10 && sum<=20) {
-            return true;
-        }
-        return false;
-    }
-
-    public static void isPositiveOrNegativePrint(int a) {
-        if (a>=0)
-            System.out.println("Число положтельное");
-        else
-            System.out.println("Число отрицательное");
-    }
-
-    public static boolean isPositiveOrNegativeReturn(int a) {
-        if (a>=0)
-            return false;
-        else
-            return true;
-    }
-
-    public static void printStringSeveralTimes(String str, int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.println(str);
-        }
-
-    }
-
-    public static boolean isLeapYear (int year) {
-        if ((year%4==0 && year%100!=0) || (year%400==0)) {
-            return true;
-        }
-        else
-            return false;
-    }
 
 }
